4591a884af25ba694b116e2c3b9ac6e69523a1e3
Olcaytp <olcaytapan10@gmail.com>
2024-10-14 15:09:49 +0200

    linkedinchanged

--- src/components/Home/Home2.js
+++ src/components/Home/Home2.js
@@ -72,7 +72,7 @@ function Home2() {
               </li>
               <li className="social-icons">
                 <a
-                  href="https://www.linkedin.com/in/olcaytapan/"
+                  href="https://www.linkedin.com/in/olcayt/"
                   target="_blank"
                   rel="noreferrer"
                   className="icon-colour  home-social-icons"
